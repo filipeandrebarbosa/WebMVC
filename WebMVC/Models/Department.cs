054e658e7c596c2d536e359c18a060c817414ca2
Filipe Barbosa <filipe.andre.barbosa@gmail.com>
2023-08-17 17:08:41 +0100

    Adaptacao SQL

--- WebMVC/Models/Department.cs
+++ WebMVC/Models/Department.cs
@@ -10,10 +10,6 @@ namespace WebMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public Department(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+
     }
 }
