cbf6d4f89d67c8d0b0d42f58ed20a15602cd9822
Filipe Barbosa <filipe.andre.barbosa@gmail.com>
2023-08-17 17:37:37 +0100

    Outras Entidades e Migracao

--- WebMVC/Data/WebMVCContext.cs
+++ WebMVC/Data/WebMVCContext.cs
@@ -14,6 +14,8 @@ namespace WebMVC.Data
         {
         }
 
-        public DbSet<WebMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
     }
 }
